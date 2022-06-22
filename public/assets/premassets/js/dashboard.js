5f064b0b3153e3318ec15d9138be7a58f01d91d8
Premalatha <premavictory98@gmail.com>
2022-06-22 16:17:51 +0530

    dashboard change

--- public/assets/premassets/js/dashboard.js
+++ public/assets/premassets/js/dashboard.js
@@ -507,25 +507,25 @@ $(document).ready(function () {
               var event_start = $.fullCalendar.formatDate(event_start, "Y-MM-DD HH:mm:ss");
               var event_end = $.fullCalendar.formatDate(event_end, "Y-MM-DD HH:mm:ss");
               $.ajax({
-                  url: SITEURL + "/calendar-crud-ajax",
-                  data: {
-                      event_name: event_name,
-                      event_start: event_start,
-                      event_end: event_end,
-                      type: 'create'
-                  },
-                  type: "POST",
-                  success: function (data) {
-                      displayMessage("Event created.");
-                      calendar.fullCalendar('renderEvent', {
-                          id: data.id,
-                          title: event_name,
-                          start: event_start,
-                          end: event_end,
-                          allDay: allDay
-                      }, true);
-                      calendar.fullCalendar('unselect');
-                  }
+                  // url: SITEURL + "/calendar-crud-ajax",
+                  // data: {
+                  //     event_name: event_name,
+                  //     event_start: event_start,
+                  //     event_end: event_end,
+                  //     type: 'create'
+                  // },
+                  // type: "POST",
+                  // success: function (data) {
+                  //     displayMessage("Event created.");
+                  //     calendar.fullCalendar('renderEvent', {
+                  //         id: data.id,
+                  //         title: event_name,
+                  //         start: event_start,
+                  //         end: event_end,
+                  //         allDay: allDay
+                  //     }, true);
+                  //     calendar.fullCalendar('unselect');
+                  // }
               });
           }
       },
@@ -533,34 +533,34 @@ $(document).ready(function () {
           var event_start = $.fullCalendar.formatDate(event.start, "Y-MM-DD");
           var event_end = $.fullCalendar.formatDate(event.end, "Y-MM-DD");
           $.ajax({
-              url: SITEURL + '/calendar-crud-ajax',
-              data: {
-                  title: event.event_name,
-                  start: event_start,
-                  end: event_end,
-                  id: event.id,
-                  type: 'edit'
-              },
-              type: "POST",
-              success: function (response) {
-                  displayMessage("Event updated");
-              }
+              // url: SITEURL + '/calendar-crud-ajax',
+              // data: {
+              //     title: event.event_name,
+              //     start: event_start,
+              //     end: event_end,
+              //     id: event.id,
+              //     type: 'edit'
+              // },
+              // type: "POST",
+              // success: function (response) {
+              //     displayMessage("Event updated");
+              // }
           });
       },
       eventClick: function (event) {
           var eventDelete = confirm("Are you sure?");
           if (eventDelete) {
               $.ajax({
-                  type: "POST",
-                  url: SITEURL + '/calendar-crud-ajax',
-                  data: {
-                      id: event.id,
-                      type: 'delete'
-                  },
-                  success: function (response) {
-                      calendar.fullCalendar('removeEvents', event.id);
-                      displayMessage("Event removed");
-                  }
+                  // type: "POST",
+                  // url: SITEURL + '/calendar-crud-ajax',
+                  // data: {
+                  //     id: event.id,
+                  //     type: 'delete'
+                  // },
+                  // success: function (response) {
+                  //     calendar.fullCalendar('removeEvents', event.id);
+                  //     displayMessage("Event removed");
+                  // }
               });
           }
       }
